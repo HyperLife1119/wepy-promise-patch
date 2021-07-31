591c0d573bb9e6dd3c42850659ae51b119d34a2c
HyperLife1119 <hyperlife1119@qq.com>
2021-08-01 01:00:42 +0800

    init: init the package

---
+++ index.js
@@ -0,0 +1,17 @@
+var g = require('./global');
+
+if (g) {
+  if (!g.Promise) {
+    g.Promise = require('promise-polyfill');
+  }
+
+  if (!g.Promise.prototype.finally) {
+    g.Promise.prototype.finally = require('promise-polyfill').prototype.finally;
+  }
+
+  if (!g.regeneratorRuntime) {
+    g.regeneratorRuntime = require('regenerator-runtime/runtime');
+  }
+} else {
+  console.warn('请确认关闭小程序选项 "关闭ES6转ES5"');
+}
